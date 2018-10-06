3ceea14b166c33c6f3ac005aac7ddcac883c98df
joshsarna <joshsarna@gmail.com>
2018-10-05 17:56:18 -0700

    finish adding_commas pseudocode

---
+++ adding_commas.rb
@@ -0,0 +1,34 @@
+# Commit 1 - Pseudocode
+=begin
+
+Note: =begin and =end are a way of commenting out an entire block of code. It is not very common, but it can be useful.
+
+input: 5
+output: "5"
+
+input: 1000000
+output: "1,000,000"
+
+input: "hello"
+output: "hello"
+
+Steps
+1 - define a method
+2 - check whether input is an integer
+3 - return the input if it's not a string and convert it to a string otherwise
+4 - insert commas every three indices from the right end, until there is only one digit left on the left
+
+=end
+
+# Commit 2 - Initial Solution
+
+def add_commas(number)
+  # write code here  
+end
+
+# Commit 4 - Refactor Solution
+
+
+
+# Commit 3 - Write Runner Code / Tests
+
