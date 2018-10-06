0539a3a42ce26c9d1b4ea016a4530607fd2b9891
joshsarna <joshsarna@gmail.com>
2018-10-05 17:46:15 -0700

    finish drive_thru refactor

--- drive_thru.rb
+++ drive_thru.rb
@@ -1,26 +1,31 @@
 
-# Initial Solution
+# Refactored Solution
+def collect_order
+  @order = gets.chomp
+end
+
+def process_order
+  if @order == "THATS IT"
+    @thats_it_counter += 1
+  end
+  if @thats_it_counter == 3
+    puts "Pull up to the first window."
+  elsif @order == @order.upcase # if you shout
+    questions = ["That comes with hash browns or fries. Which do you want?", "Do you want cheese on that?", "Do you want to supersize that?", "Hey, is this Paul?", "And then?", "What else?", "Do you think I could make it as a radio talk show host?"]
+    puts questions.sample
+  else # order is not shouted
+    puts "I'm sorry, you'll have to speak up Ma'am."
+  end
+end
+
 def drive_thru
   puts "Welcome to Ruby Burger. We have specials on Hamburgers, Fish Filet and Cookies for a dollar. Can I take your order?"
-  order = ""
-  thats_it_counter = 0
-  while thats_it_counter < 3 # until thats_it_counter == 3
-    order = gets.chomp
-    if order == "THATS IT"
-      thats_it_counter += 1
-    end
-    # incrimenting the counter and responding are in separate if statements because we want to be able to increase the counter and respond in the same time through the loop that begins on line 7
-    if thats_it_counter == 3
-      puts "Pull up to the first window."
-    elsif order == order.upcase # if you shout
-      # we have an array of prewritten questions
-      questions = ["That comes with hash browns or fries. Which do you want?", "Do you want cheese on that?", "Do you want to supersize that?", "Hey, is this Paul?", "And then?", "What else?", "Do you think I could make it as a radio talk show host?"]
-      # random question asked
-      puts questions.sample
-    else # order is not shouted
-      # ask for louder voice
-      puts "I'm sorry, you'll have to speak up Ma'am."
-    end
+  # order and thats_it_counter must now be instance variables since they're being used in more than one method
+  @order = ""
+  @thats_it_counter = 0
+  while @thats_it_counter < 3
+    collect_order
+    process_order
   end
 end
 
