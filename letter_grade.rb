27b4a105d2078dc8c1f52dee76416b005729d286
joshsarna <joshsarna@gmail.com>
2018-10-07 11:22:10 -0700

    finish letter_grade pseudocode

---
+++ letter_grade.rb
@@ -0,0 +1,21 @@
+# Commit 1 - Pseudocode
+
+# input: 90, 100, 95
+# output: "A"
+
+# Steps
+# 1 - define a method that can accept an arbitrary number of arguments
+# 2 - find the average of those arguments (sum them and then divide by the number of them)
+# 3 - attribute the average to a letter grade using conditionals
+
+# Commit 2 - Initial Solution
+
+def calculate_letter_grade()
+  # write code here  
+end
+
+# Commit 4 - Refactor Solution
+
+
+
+# Commit 3 - Write Runner Code / Tests
