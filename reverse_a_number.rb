2842ebdb212b67efa4c2197e06e4478671c06336
joshsarna <joshsarna@gmail.com>
2018-10-06 19:30:48 -0700

    finish reverse_a_number no strings

--- reverse_a_number.rb
+++ reverse_a_number.rb
@@ -12,7 +12,13 @@
 # Commit 2 - Initial Solution
 
 def reverse_a_number(number)
-  # write code here  
+  output_number = 0
+  while number != 0 do
+    output_number *= 10
+    output_number += (number % 10)
+    number = (number / 10)  # => this is an easy way to chop off the last (right-most) digit of an integer
+  end
+  output_number
 end
 
 # Commit 3 - No Strings Solution
@@ -25,7 +31,7 @@ end
 
 # Runner Code / Tests
 
-puts reverse_a_number(67352193) == 39125376
-puts reverse_a_number(97531) == 13579
-puts reverse_a_number(24) == 42
-puts reverse_a_number(1574362190881) == 1880912634751
+puts reverse_a_number(67352193) == 39125376  # => true
+puts reverse_a_number(97531) == 13579  # => true
+puts reverse_a_number(24) == 42  # => true
+puts reverse_a_number(1574362190881) == 1880912634751  # => true
