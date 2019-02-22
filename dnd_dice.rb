b8b50a0dff51749c18dae91410ffad559ce52a17
joshsarna <joshsarna@gmail.com>
2019-02-22 15:10:48 -0800

    finish dnd_dice pseudocode

--- dnd_dice.rb
+++ dnd_dice.rb
@@ -1,5 +1,14 @@
 # Commit 1 - Psuedocode
 
+# make a Die class
+# make initialize method
+  # accept number of sides as a parameter
+# make sides getter
+  # return integer
+# make roll getter
+  # grab sides attribute
+  # generate a random number between 1 and the number of sides
+  # return integer
 
 
 # Commit 2 and 4 - Initial Solution
