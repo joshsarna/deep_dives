dad499b6f0842e98b3ede0ec830261dcd3a60434
joshsarna <joshsarna@gmail.com>
2019-02-27 01:23:50 -0800

    refactor looney_tunes porky pig solution

--- looney_tunes.rb
+++ looney_tunes.rb
@@ -27,24 +27,44 @@ def daffify(message)
   methage = methage.gsub('thth', 'th')
 end
 
-def porkify(message, dictionary)
+# def porkify(message, dictionary)
+#   vowels = ["a", "e", "i", "o", "u", "y"]
+#   words = message.split(" ")
+#   words.each do |word|
+#     if dictionary.include?(word)
+#       if vowels.include?(word[1])
+#         word.gsub!(word.gsub!(/[^a-z]/, ''), "eh #{word[0]}eh #{word[0].upcase}eh eh #{word}")
+#       else
+#         word.gsub!(word.gsub(/[^a-z]/, ''), "eh #{word[0..1]} #{word[0..1].upcase} eh #{word}")
+#       end
+#     end
+#   end
+#   porked_message = words.join(' ')
+#   p porked_message
+# end
+
+# Commit 4 - Refactor Solution
+def stutter(word)
   vowels = ["a", "e", "i", "o", "u", "y"]
-  words = message.split(" ")
-  words.each do |word|
-    if dictionary.include?(word)
-      if vowels.include?(word[1])
-        word.gsub!(word.gsub!(/[^a-z]/, ''), "eh #{word[0]}eh #{word[0].upcase}eh eh #{word}")
-      else
-        word.gsub!(word.gsub(/[^a-z]/, ''), "eh #{word[0..1]} #{word[0..1].upcase} eh #{word}")
-      end
+  stuttered_word_without_punctuation = "eh #{word[0..1]} #{word[0..1].upcase} eh #{word}"
+  if vowels.include?(word[1])
+    stuttered_word_without_punctuation[4] = "eh"
+    stuttered_word_without_punctuation[8] = "EH"
+  end
+  stuttered_word = word.gsub(word, stuttered_word_without_punctuation)
+  return stuttered_word
+end
+
+def porkify(message, dictionary)
+  words = message.split(' ')
+  words.each_with_index do |word, i|
+    word_without_punctuation = word.gsub(/\W/, '')
+    if dictionary.include?(word_without_punctuation)
+      words[i] = stutter(word)
     end
   end
   porked_message = words.join(' ')
-  p porked_message
 end
-
-# Commit 4 - Refactor Solution
-
 
 #Commit 3 - Write Runner Code / Tests
 
@@ -59,3 +79,4 @@ end
 # porky pig runner code
 dictionary = ["this", "place", "tomorrow", "rust"]
 p porkify("this is a nice place", dictionary) == "eh th TH eh this is a nice eh pl PL eh place"
+p porkify("this is a nice rust.", dictionary) == "eh th TH eh this is a nice eh reh REH eh rust."
