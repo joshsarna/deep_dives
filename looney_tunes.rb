330b9ee7396a01ba4ab82f6cd807b10a8708feb5
joshsarna <joshsarna@gmail.com>
2019-02-26 23:34:05 -0800

    finish looney_tunes pseudocode for daffy duck

---
+++ looney_tunes.rb
@@ -0,0 +1,25 @@
+#Commit 1 - Pseudocode
+
+# daffy duck
+# input: "This is ridiculous" (string)
+# output: "Thith ith ridiculouth" (string)
+
+# define a method that accepts a string as a parameter
+# create a new string
+# use gsub to replace 's' with 'th'
+# return the new string
+
+#Commit 2 - Initial Solution
+
+def daffify(message)
+  # write code here  
+end
+
+def porkify(message, dictionary)
+  # write code here  
+end
+
+# Commit 4 - Refactor Solution
+
+
+#Commit 3 - Write Runner Code / Tests
